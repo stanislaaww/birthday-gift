6eda723eea51649d864482a06ce8007430a742e8
stanislaaww <stasgrecukov8@gmail.com>
2026-04-27 20:07:10 +0500

    Update fallback.js

--- client/src/data/fallback.js
+++ client/src/data/fallback.js
@@ -51,7 +51,7 @@ export const fallbackContent = {
     venue: 'Достык'
   },
 
-  finalMessage: 'Я надеюсь, тебе понравилось \nС Днём Рождения',
+  finalMessage: 'Надеюсь, тебе понравилось \nС Днём Рождения',
 
   musicSrc: '/music/music.mp3'
 };
